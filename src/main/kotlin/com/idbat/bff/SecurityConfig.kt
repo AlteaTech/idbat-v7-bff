b7b54268b2df28ab7d3f45eac947b4abeb01ce73
Jeremie Vidal <jeremie.vidal13@gmail.com>
2026-03-20 11:44:12 +0100

    sprint 2 fixs

--- src/main/kotlin/com/idbat/bff/SecurityConfig.kt
+++ src/main/kotlin/com/idbat/bff/SecurityConfig.kt
@@ -6,16 +6,19 @@ import org.springframework.security.authentication.ReactiveAuthenticationManager
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableWebFluxSecurity
 class SecurityConfig(
     private val customAuthenticationManager: ReactiveAuthenticationManager
 ) {
-
     @Bean
     fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
         return http
+            .cors { it.disable() }
             .csrf { it.disable() }
             .authorizeExchange { exchanges ->
                 exchanges
@@ -24,11 +27,9 @@ class SecurityConfig(
                         "/swagger-ui/**",
                         "/v3/api-docs/**",
                         "/webjars/**",
-                        "/api-front/v3/api-docs/**"
+                        "/api-front/v3/api-docs/**",
+                        "/front/**"
                     ).permitAll()
-                    .pathMatchers("/actuator/**").permitAll()
-                    .pathMatchers("/api-front/api/auth/login").permitAll()
-                    .anyExchange().authenticated()
             }
             .oauth2ResourceServer { oauth2 ->
                 // On utilise notre CustomAuthenticationManager au lieu du JwtDecoder standard
